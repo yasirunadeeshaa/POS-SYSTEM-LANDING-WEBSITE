290ef4cefc26cfcf8b55ffa031ab93128b2c46bd
A . Yasiru Nadeesha Aththanayaka <a.y.nadeeshaaththanayaka@gmail.com>
2026-03-05 22:37:17 +0530

    update

--- pos-system/src/routes/AppRoutes.jsx
+++ pos-system/src/routes/AppRoutes.jsx
@@ -1,5 +1,9 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+
 import HeroSection from '../pages/main/Herosection'
+import FeatureSection from '../pages/main/Feature'
+import Navigation from '../components/Navigation'
+import Footer from '../components/Footer'
 import NotFound from '../pages/Notfound'
 
 // Simple auth guard — replace with your real auth logic later
@@ -10,20 +14,53 @@ const PrivateRoute = ({ children }) => {
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      {/* Public Routes */}
-      <Route path="/"   element={<HeroSection />} />
+    // <Routes>
+    //   {/* Public Routes */}
+    //   <Route path="/"   element={<HeroSection />} />
+    //   <Route path="/features"   element={<FeatureSection />} />
 
-      {/* Protected Routes */}
-      {/* <Route path="/dashboard" element={
-        <PrivateRoute>
-          <Dashboard />
-        </PrivateRoute>
-      } /> */}
+    //   {/* Protected Routes */}
+    //   {/* <Route path="/dashboard" element={
+    //     <PrivateRoute>
+    //       <Dashboard />
+    //     </PrivateRoute>
+    //   } /> */}
 
-      {/* 404 */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    //   {/* 404 */}
+    //   <Route path="*" element={<NotFound />} />
+    // </Routes>
+    <div className="app">
+        <Navigation />
+        <Routes>
+          {/* Main Portfolio Page */}
+          <Route path="/" element={
+            <>
+              <section id="">
+                <HeroSection />
+              </section>
+              <section id="features">
+                <FeatureSection />
+              </section>
+              {/*<section id="projects">
+                <MyProject />
+              </section>
+              <section id="skills">
+                <Skill />
+              </section>
+              <section id="testimonials">
+                <PortfolioTestimonials />
+              </section>
+              <section id="contact">
+                <Contact />
+              </section> */}
+              
+            </>
+          } />
+          <Route path="*" element={<NotFound />} />
+          {/* <Route path="/projects/healthcare" element={<HealthcareProjectDetail />} /> */}
+        </Routes>
+        <Footer />
+      </div>
   )
 }
 
