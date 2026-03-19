f975385bde5668f426fde266674add0bfa8a4d55
A . Yasiru Nadeesha Aththanayaka <a.y.nadeeshaaththanayaka@gmail.com>
2026-03-19 21:32:05 +0530

    Update hero section

--- src/pages/main/Herosection.jsx
+++ src/pages/main/Herosection.jsx
@@ -179,7 +179,7 @@ export default function HeroSection() {
             </div>
 
             <div className="actions">
-              <button className="btn-p" onClick={() => navigate("/admin/dashboard")}>
+              <button className="btn-p" onClick={() => navigate("/login")}>
                 Explore Features
                 <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round">
                   <path d="M5 12h14M12 5l7 7-7 7" />
