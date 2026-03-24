d717b871e2cb4d2a875117830c50ecb916182d48
A . Yasiru Nadeesha Aththanayaka <a.y.nadeeshaaththanayaka@gmail.com>
2026-03-24 22:08:01 +0530

    Update app routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,26 +1,48 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
+//main pages
 import HeroSection from "../pages/main/Herosection";
 import FeatureSection from "../pages/main/Feature";
 import Navigation from "../components/Navigation";
 import ModuleSection from "../pages/main/ModuleSection";
 import HowItWorks from "../pages/main/HowItWorks";
 import Footer from "../components/Footer";
-import NotFound from "../pages/Notfound";
-import Analysis from "../pages/Analysis";
+import ComparisonSection from "../pages/main/Comparisonsection";
+
+//auth
+import Login from "../pages/auth/Login";
+
+//dashboard
+import Dashboard from "../pages/dashboards/PosDashboard";
+
+//Inventory
+import Quotation from "../pages/sales/Quotation";
+import QuotationSummery from "../pages/sales/QuotationSummaryModel";
+import QuotationHistory from "../pages/history/QutationHistory";
+import ProductListView from "../pages/product/ProductManagement";
+import ProductSelectionModal from "../pages/product/ProductSelection";
+import CategoryManagement from "../pages/product/CaregoryManagement";
+import PurchaseOrder from "../pages/product/PurchaseOrder";
 import Invoice from "../pages/sales/Invoice";
 import AddProduct from "../pages/product/Addproduct";
 import InvoiceHistory from "../pages/sales/InvoiceHistory";
-import ComparisonSection from "../pages/main/Comparisonsection";
-import ProductListView from "../pages/product/ProductListView";
-import CategoryManagement from "../pages/CaregoryManagement";
-import CustomerManagement from "../pages/customers/CustomerManagement";
+import GRN from "../pages/sales/GRN";
+import PurchaseOrderHistory from "../pages/history/PurchaseOrderHistory";
+import CreditNote from "../pages/sales/CreditNote";
+import DebitNote from "../pages/sales/DebitNote";
 
+//Customer
+import CustomerManagement from "../pages/customers/CustomerManagement";
+import AddCustomer from "../pages/customers/AddCustomer";
+
+//supplier
 import SupplierManagement from "../pages/suppliers/SupplierManagement";
 import SupplierAccount from "../pages/suppliers/SupplierAccount";
 
-import AdminDashboard from "../pages/dashboards/AdminDashboard";
+import Transactions from "../pages/other/Transactions";
+import Analysis from "../pages/other/Analysis";
 
+import NotFound from "../pages/Notfound";
 import Demo from "../pages/Demo";
 
 const AppRoutes = () => {
@@ -56,7 +78,8 @@ const AppRoutes = () => {
       />
 
       {/* Admin Dashboard (No Navigation / Footer) */}
-      <Route path="/admin/dashboard" element={<AdminDashboard />}/>
+      <Route path="/dashboard" element={<Dashboard />}/>
+      <Route path="/login" element={<Login />}/>
 
       <Route path="/analysis" element={<Analysis />}/>
       <Route path="/invoice" element={<Invoice />} />
@@ -65,11 +88,22 @@ const AppRoutes = () => {
       <Route path="/invoicesummary" element={<invoiceSummaryModal />} />
       <Route path="/addproduct" element={<AddProduct />} />
       <Route path="/productsListView" element={<ProductListView />} />
+      <Route path="/productSelection" element={<ProductSelectionModal />} />
       <Route path="/categoryManagement" element={<CategoryManagement />} />
       <Route path="/customerManagement" element={<CustomerManagement />} />
+      <Route path="/addCustomer" element={<AddCustomer />} />
       <Route path="/supplierManagement" element={<SupplierManagement />} />
       <Route path="/supplierAccount" element={<SupplierAccount />} />
       <Route path="/demo" element={<Demo />} />
+      <Route path="/transactions" element={<Transactions />} />
+      <Route path="/quotation" element={<Quotation />} />
+      <Route path="/quotationSummary" element={<QuotationSummery />} />
+      <Route path="/quotationHistory" element={<QuotationHistory />} />
+      <Route path="/grn" element={<GRN />} />
+      <Route path="/purchase-order" element={<PurchaseOrder />} />
+      <Route path="/purchase-order-history" element={<PurchaseOrderHistory />} />
+      <Route path="/credit-note" element={<CreditNote />} />
+      <Route path="/debit-note" element={<DebitNote />} />
       {/* 404 */}
       <Route path="*" element={<NotFound />} />
 
